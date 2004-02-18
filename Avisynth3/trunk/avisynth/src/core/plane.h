7f55ae5c5e3b232ce6018b262be75b66d59bc183
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-02-18 09:45:13 +0000

    Plane enum

---
+++ Avisynth3/trunk/avisynth/src/core/plane.h
@@ -0,0 +1,50 @@
+// Avisynth v3.0 alpha.  Copyright 2003 Ben Rudiak-Gould et al.
+// http://www.avisynth.org
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+// http://www.gnu.org/copyleft/gpl.html .
+//
+// Linking Avisynth statically or dynamically with other modules is making a
+// combined work based on Avisynth.  Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+
+
+#ifndef  __AVS_PLANE_H__
+#define  __AVS_PLANE_H__
+
+
+
+namespace avs {
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+//  Plane
+//
+//  enum of possible plane values
+//
+enum Plane
+{
+  NOT_PLANAR,
+  PLANAR_Y,
+  PLANAR_U,
+  PLANAR_V
+};
+
+
+
+} //namespace avs
+
+#endif  //__AVS_PLANE_H__
+

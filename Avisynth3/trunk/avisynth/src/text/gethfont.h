17c1ebe94ae02bc80e3cad74f5d0ee5f2026eafb
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-05-07 07:54:34 +0000

    GetHFONT helper functor

---
+++ Avisynth3/trunk/avisynth/src/text/gethfont.h
@@ -0,0 +1,56 @@
+// Avisynth v3.0 alpha.  Copyright 2004 Ben Rudiak-Gould et al.
+// http://www.avisynth.org
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+// http://www.gnu.org/copyleft/gpl.html .
+//
+// Linking Avisynth statically or dynamically with other modules is making a
+// combined work based on Avisynth.  Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+
+
+#ifndef __AVS_TEXT_GETHFONT_H__
+#define __AVS_TEXT_GETHFONT_H__
+
+//avisynth include
+#include "font.h"
+
+//windows include
+#include <windows.h>
+#include <wingdi.h>
+
+
+namespace avs { namespace text {
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+//  GetHFONT
+//
+//  functor to retrieve the underlying HFONT from a Font object
+//  part of the trick hidding win headers from font.h
+//
+struct GetHFONT
+{
+
+  HFONT operator()(Font const& font) const { return *(HFONT *)font.pFont_.get(); }
+
+};
+
+
+
+} } //namespace avs::text
+
+#endif //__AVS_TEXT_GETHFONT_H__

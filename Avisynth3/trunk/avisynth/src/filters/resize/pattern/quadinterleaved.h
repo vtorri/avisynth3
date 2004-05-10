81ad877b5c2e7bcab2920df6075fff4bd2182cf7
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-05-10 08:16:21 +0000

    QuadInterleaved pattern

---
+++ Avisynth3/trunk/avisynth/src/filters/resize/pattern/quadinterleaved.h
@@ -0,0 +1,55 @@
+// Avisynth v3.0 alpha.  Copyright 2004 Ben Rudiak-Gould et al.
+// http://www.avisynth.org
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+// http://www.gnu.org/copyleft/gpl.html .
+//
+// Linking Avisynth statically or dynamically with other modules is making a
+// combined work based on Avisynth.  Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+
+
+#ifndef __AVS_FILTERS_RESIZE_PATTERN_QUADINTERLEAVED_H__
+#define __AVS_FILTERS_RESIZE_PATTERN_QUADINTERLEAVED_H__
+
+//avisynth include
+#include "base.h"
+#include "../forward.h"
+
+
+namespace avs { namespace filters { namespace resize { namespace pattern {
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+//  QuadInterleaved
+//
+//
+//
+class QuadInterleaved : public Base
+{
+
+public:  //constructor
+
+  QuadInterleaved(PEnvironment const& env, Filter const& filter, SubRange const& subrange, int size);
+
+};
+
+
+
+
+} } } } //namespace avs::filters::resize::pattern
+
+#endif //__AVS_FILTERS_RESIZE_PATTERN_QUADINTERLEAVED_H__

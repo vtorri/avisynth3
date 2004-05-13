5fe2114d87b89c9932451bc85bc38ce7cb6538bb
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-05-13 13:55:04 +0000

    removed useless Recycler typedef

--- Avisynth3/trunk/avisynth/src/core/block/base.h
+++ Avisynth3/trunk/avisynth/src/core/block/base.h
@@ -1,4 +1,4 @@
-// Avisynth v3.0 alpha.  Copyright 2003 Ben Rudiak-Gould et al.
+// Avisynth v3.0 alpha.  Copyright 2004 Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
 // This program is free software; you can redistribute it and/or modify
@@ -49,7 +49,6 @@ template <class Deleter> class base
   
 public:  //typedefs
 
-  typedef Recycler Recycler;
   typedef unsigned char BYTE;
 
 

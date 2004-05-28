f3be430f44d6da443860dd3ae5f54f3d7ce50fb0
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-05-28 18:16:44 +0000

    remplaced use of std::pair by boost::tuple

--- Avisynth3/trunk/avisynth/src/parser/lazy/tuple.h
+++ Avisynth3/trunk/avisynth/src/parser/lazy/tuple.h
@@ -21,11 +21,12 @@
 // General Public License cover the whole combination.
 
 
-#ifndef __AVS_PARSER_LAZY_PAIR_H__
-#define __AVS_PARSER_LAZY_PAIR_H__
+#ifndef __AVS_PARSER_LAZY_TUPLE_H__
+#define __AVS_PARSER_LAZY_TUPLE_H__
 
 //boost includes
 #include <boost/ref.hpp>
+#include <boost/tuple/tuple.hpp>
 #include <boost/spirit/phoenix/functions.hpp>
 
 
@@ -33,19 +34,13 @@ namespace avs { namespace parser { namespace lazy {
 
 
 
-struct FirstAttribute
+template <int n> struct Attribute
 {
 
   template <typename T>
   struct result
   {
-    typedef typename T::first_type& type;
-  };
-
-  template <typename T>
-  struct result<T const>
-  {
-    typedef typename T::first_type const& type;
+    typedef typename boost::tuples::element<n, T>::type& type;
   };
 
   template <typename T>
@@ -70,72 +65,24 @@ struct FirstAttribute
   template <typename T>
   typename result<T>::type operator()(T& val) const
   { 
-    return val.first;
+    return val.get<n>();
   }
 
   template <typename T>
   typename result<T>::type operator()(boost::reference_wrapper<T> val) const
   {
-    return val.get().first;
+    return val.get().get<n>();
   }
 
 };
 
 
+phoenix::function<Attribute<0> > const first = Attribute<0>();
+phoenix::function<Attribute<1> > const second = Attribute<1>();
+phoenix::function<Attribute<2> > const third = Attribute<2>();
 
-struct SecondAttribute
-{
-
-  template <typename T>
-  struct result
-  {
-    typedef typename T::second_type& type;
-  };
-
-  template <typename T>
-  struct result<T const>
-  {
-    typedef typename T::second_type const& type;
-  };
-
-  template <typename T>
-  struct result<boost::reference_wrapper<T> >
-  {
-    typedef typename result<T>::type type;
-  };
-
-  template <typename T>
-  struct result<boost::reference_wrapper<T> const>
-  {
-    typedef typename result<T>::type type;
-  };
-
-  //deflect phoenix::nil_t... I dunno why it's necessary
-  template <>
-  struct result<phoenix::nil_t>
-  {
-    typedef phoenix::nil_t type;
-  };
-
-  template <typename T>
-  typename result<T>::type operator()(T& val) const
-  { 
-    return val.second;
-  }
-
-  template <typename T>
-  typename result<T>::type operator()(boost::reference_wrapper<T> val) const
-  {
-    return val.get().second;
-  }
-
-};
-
-
-phoenix::function<FirstAttribute> const first = FirstAttribute();
-phoenix::function<SecondAttribute> const second = SecondAttribute();
 
 
 } } } //namespace avs::parser::lazy
 
-#endif //__AVS_PARSER_LAZY_PAIR_H__
+#endif //__AVS_PARSER_LAZY_TUPLE_H__

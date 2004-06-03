007a3852e1a030c272d3b8b5de18d24f3603bd68
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-06-03 12:54:04 +0000

    gcc is an idiot fix + removed pragma warning

--- Avisynth3/trunk/avisynth/src/filters/transform/turn.h
+++ Avisynth3/trunk/avisynth/src/filters/transform/turn.h
@@ -1,4 +1,4 @@
-// Avisynth v3.0 alpha.  Copyright 2003 Ben Rudiak-Gould et al.
+// Avisynth v3.0 alpha.  Copyright 2004 Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
 // This program is free software; you can redistribute it and/or modify
@@ -28,10 +28,6 @@
 #include "../../clip/caching/concrete.h"
 #include "../../clip/onechild/concrete.h"
 #include "../../clip/onechild/cachingpipeline.h"
-
-
-#pragma warning ( push )           //push warning state
-#pragma warning (disable : 4250)   //get rid of MI dominance decisions
 
 
 namespace avs { namespace filters {
@@ -185,7 +181,5 @@ public:  //factory method
 
 
 } } //namespace avs::filters
-
-#pragma warning ( pop )
 
 #endif //__AVS_FILTERS_TURN_H__

9dae1371202b93179c7b263a947be217eb9845c3
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-06-04 10:24:24 +0000

    custom VarTable symbosl table

---
+++ Avisynth3/trunk/avisynth/src/parser/grammar/vartable.h
@@ -0,0 +1,77 @@
+// Avisynth v3.0 alpha.  Copyright 2004 Ben Rudiak-Gould et al.
+// http://www.avisynth.org
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+// http://www.gnu.org/copyleft/gpl.html .
+//
+// Linking Avisynth statically or dynamically with other modules is making a
+// combined work based on Avisynth.  Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+
+
+#ifndef __AVS_PARSER_GRAMMAR_VARTABLE_H__
+#define __AVS_PARSER_GRAMMAR_VARTABLE_H__
+
+//boost include
+#include <boost/spirit/symbols.hpp>
+
+namespace avs { namespace parser { namespace grammar {
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+//  VarTable
+//
+//  symbols mapping var names to their type and position in the stack
+//
+class VarTable : public spirit::symbols<TypedIndex>
+{
+
+  int size_;       //NB: not the number of symbols defined
+
+
+public:  //structors
+
+  VarTable()
+    : size_( 0 ) { }
+
+  VarTable(VarTable const& other)
+    : spirit::symbols<TypedIndex>( other )
+    , size_( other.size_ ) { }
+
+  //generated destructor is fine
+
+
+public:  //VarTable interface
+
+  void DefineVar(std::string const& name, char type)
+  {
+    TypedIndex value(size_, type);
+
+    if ( TypedIndex * index = find(*this, name.c_str()) )   //if already defined
+      *index = value;                                       //we override value
+    else add(name.c_str(), value);                          //else we add it
+
+    ++size_;
+  }
+
+  int size() const { return size_; }
+
+};
+
+
+} } } //namespace avs::parser::grammar
+
+#endif //__AVS_PARSER_GRAMMAR_VARTABLE_H__

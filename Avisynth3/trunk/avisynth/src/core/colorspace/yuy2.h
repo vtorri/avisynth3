e9585c07487d7c68d73a8125dbfe8eeaa952d5c7
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-06-06 19:53:37 +0000

    forgot to remove some code

--- Avisynth3/trunk/avisynth/src/core/colorspace/yuy2.h
+++ Avisynth3/trunk/avisynth/src/core/colorspace/yuy2.h
@@ -1,4 +1,4 @@
-// Avisynth v3.0 alpha.  Copyright 2003 Ben Rudiak-Gould et al.
+// Avisynth v3.0 alpha.  Copyright 2004 Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
 // This program is free software; you can redistribute it and/or modify
@@ -67,11 +67,6 @@ public:  //ColorSpace interface
   {
     return CPVideoFrame( (VideoFrame *)new vframe::YUY2(dim, type, env) );
   }
-
-
-public:  //Interleaved interface
-
-  virtual BytesPerPixel() const { return 2; }
 
 
 public: 

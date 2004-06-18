3869155f22ded869a641c9146914720a74e98fcc
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-06-18 18:41:20 +0000

    gcc is an idiot fix

--- Avisynth3/trunk/avisynth/src/text/align.h
+++ Avisynth3/trunk/avisynth/src/text/align.h
@@ -1,4 +1,4 @@
-// Avisynth v3.0 alpha.  Copyright 2004 Ben Rudiak-Gould et al.
+// Avisynth v3.0 alpha.  Copyright 2004 David Pierre - Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
 // This program is free software; you can redistribute it and/or modify
@@ -32,7 +32,7 @@ namespace avs { namespace text {
 ///////////////////////////////////////////////////////////////////////////////////
 //  Align
 //
-//  enumerates how text can be aligned
+//  enumerates how text can be aligned, match placement on keyboard numpad
 //
 enum Align
 {

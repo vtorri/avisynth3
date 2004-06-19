21dd635561cec74bd663affeec2023e884b264a7
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-06-19 14:35:12 +0000

    no message

--- Avisynth3/trunk/avisynth/src/filters/resize/pattern/base.h
+++ Avisynth3/trunk/avisynth/src/filters/resize/pattern/base.h
@@ -1,4 +1,4 @@
-// Avisynth v3.0 alpha.  Copyright 2004 Ben Rudiak-Gould et al.
+// Avisynth v3.0 alpha.  Copyright 2004 David Pierre - Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
 // This program is free software; you can redistribute it and/or modify
@@ -74,7 +74,7 @@ protected:  //assignment
 public:  //access
 
   //gets pattern data
-  int const * get() const { return (int const *)pattern_.get(); }
+  int const * get() const { return reinterpret_cast<int *>(pattern_.get()); }
 
   //always return the coeffs count per pixel, not the number of loops expected    
   int count() const { return count_; }
@@ -84,7 +84,7 @@ protected:  //at the disposition of subclasses constructor
 
   void init(int count, int size) { count_ = count; pattern_.reset(size << 2, false); }
 
-  int * get() { return (int *)pattern_.get(); }
+  int * get() { return reinterpret_cast<int *>(pattern_.get()); }
 
 };
 

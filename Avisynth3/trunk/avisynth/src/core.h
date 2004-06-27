e040d1ad75e4033584ef2d9985ec3772b42e233d
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-06-27 10:53:11 +0000

    master include for /core folder

---
+++ Avisynth3/trunk/avisynth/src/core.h
@@ -0,0 +1,36 @@
+// Avisynth v3.0 alpha.  Copyright 2004 David Pierre - Ben Rudiak-Gould et al.
+// http://www.avisynth.org
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+// http://www.gnu.org/copyleft/gpl.html .
+//
+// Linking Avisynth statically or dynamically with other modules is making a
+// combined work based on Avisynth.  Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+
+
+#ifndef __AVS_CORE_H__
+#define __AVS_CORE_H__
+
+//avisynth includes
+#include "avs/clip.h"
+#include "avs/blitter.h"
+#include "avs/videoinfo.h"
+#include "avs/colorspace.h"
+#include "avs/videoframe.h"
+#include "avs/cow_shared_ptr.h"
+#include "avs/runtime_environment.h"
+
+#endif //__AVS_CORE_H__

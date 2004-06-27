516f320c568a2f954ae65ae33a05b69a75594fba
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-06-27 19:59:13 +0000

    now search asm files too

--- Avisynth3/trunk/avisynth/build/linux/gen_sources.sh
+++ Avisynth3/trunk/avisynth/build/linux/gen_sources.sh
@@ -4,3 +4,6 @@ echo SRC_DIR = ../../src > sources.inc
 echo   >> sources.inc
 echo SRC_GENERIC = \\ >> sources.inc
 sed '/com\\/d;/vfw\\/d;/cpp/!d;s/^[ \t]*//;s/RelativePath=".\\src\\/  /;s/\\/\//g;s/">/ \\/' ../../avisynth.vcproj >>sources.inc
+echo   >> sources.inc
+echo SRC_IA32 = \\ >> sources.inc
+find ../../src/ -name '*x86.asm' | sed 's/..\/..\/src\//  /;s/\\/\//g;s/asm/asm \\/' >>sources.inc

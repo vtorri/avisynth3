6d8ebff3ec2ad9bf35630bce6af26156400b0530
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-07-01 08:48:45 +0000

    x86.asm became x86_nasm.asm

--- Avisynth3/trunk/avisynth/build/linux/gen_sources.sh
+++ Avisynth3/trunk/avisynth/build/linux/gen_sources.sh
@@ -6,4 +6,4 @@ echo SRC_GENERIC = \\ >> sources.inc
 sed '/com\\/d;/vfw\\/d;/cpp/!d;s/^[ \t]*//;s/RelativePath=".\\src\\/  /;s/\\/\//g;s/">/ \\/' ../../avisynth.vcproj >>sources.inc
 echo   >> sources.inc
 echo SRC_IA32 = \\ >> sources.inc
-find ../../src/ -name '*x86.asm' | sed 's/..\/..\/src\//  /;s/\\/\//g;s/asm/asm \\/' >>sources.inc
+find ../../src/ -name '*x86_nasm.asm' | sed 's/..\/..\/src\//  /;s/\\/\//g;s/asm/asm \\/' >>sources.inc

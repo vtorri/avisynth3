81b8867efffa310617d75e02bb526653507bfc74
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-07-08 15:09:53 +0000

    no message

---
+++ Avisynth3/trunk/avisynth/src/filters/convert/convert.cpp
@@ -0,0 +1,57 @@
+// Avisynth v3.0 alpha.  Copyright 2004 David Pierre - Ben Rudiak-Gould et al.
+// http://www.avisynth.org
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+// http://www.gnu.org/copyleft/gpl.html .
+//
+// Linking Avisynth statically or dynamically with other modules is making a
+// combined work based on Avisynth.  Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+
+
+//avisynth includes
+#include "convert.h"
+#include "../../core/videoinfo.h"
+#include "../../core/videoframe.h"
+
+
+namespace avs { namespace filters {
+
+
+
+Convert::Convert(PClip const& child, ColorSpace& target)
+  : clip::onechild::Concrete( child )
+  , clip::caching::Concrete( child->GetEnvironment() )
+{
+  PVideoInfo vi = child->GetVideoInfo();   //start from child VideoInfo
+  
+  vi->SetColorSpace( target );             //change colorspace (throw on incompatibilities
+
+  vi_ = vi;                                //save it
+}
+
+
+CPVideoFrame Convert::MakeFrame(CPVideoFrame const& source) const
+{
+  PVideoFrame target = GetEnvironment()->CreateFrame(*vi_, source->GetType());
+
+  ConvertFrame(*source, *target);
+
+  return target;
+}
+
+
+
+} } //namespace avs::filters

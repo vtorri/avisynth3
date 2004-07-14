d27831424a14cc0234fbad5d4a98d9f7607324d6
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-07-14 13:05:17 +0000

    added a virtual destructor to please gcc users

--- Avisynth3/trunk/avisynth/src/parser/vmoperation.h
+++ Avisynth3/trunk/avisynth/src/parser/vmoperation.h
@@ -1,4 +1,4 @@
-// Avisynth v3.0 alpha.  Copyright 2004 Ben Rudiak-Gould et al.
+// Avisynth v3.0 alpha.  Copyright 2004 David Pierre - Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
 // This program is free software; you can redistribute it and/or modify
@@ -42,6 +42,7 @@ namespace detail {
 template <typename Result>
 struct CallBack
 {
+  virtual ~CallBack() { }
   virtual Result operator()(VMState& state) const = 0;
 };
 

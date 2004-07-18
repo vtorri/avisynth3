274f724c53b9314083c5946ffb24fabcc8a1bcaa
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-07-18 21:30:11 +0000

    no message

---
+++ Avisynth3/trunk/avisynth/src/freetype/face.h
@@ -0,0 +1,81 @@
+// Avisynth v3.0 alpha.  Copyright 2004 David Pierre - Ben Rudiak-Gould et al.
+// http://www.avisynth.org
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+// http://www.gnu.org/copyleft/gpl.html .
+//
+// Linking Avisynth statically or dynamically with other modules is making a
+// combined work based on Avisynth.  Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+
+
+#ifndef __AVS_FREETYPE_FACE_H__
+#define __AVS_FREETYPE_FACE_H__
+
+//boost include
+#include <boost/shared_ptr.hpp>
+
+// STL include
+#include <string>
+
+
+//declaration
+struct FT_FaceRec_;
+
+
+namespace avs { namespace freetype {
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+//  freetype::Face
+//
+//  C++ wrapper around the FT_FACE handle
+//
+class Face
+{
+    
+  boost::shared_ptr<FT_FaceRec_> face_;  //underlying handle
+
+
+public:  //structors
+    
+  Face(std::string const& fileName, int index);
+
+  //generated copy constructor and destructor are fine
+
+
+public:  //Basic API
+
+/*  void SetCharSize(Dimension const& size, Dimension const& resolution);
+    
+  void SetPixelSize(Dimension const& dim);
+    
+  void LoadGlyph(unsigned glyphIndex, int loadFlags);
+    
+  void LoadChar(unsigned charCode, int loadFlags);*/
+       
+//  Vecteur GetKerning(unsigned leftGlyph, unsigned rightGlyph, unsigned kerningMode);
+
+  //std::string GetGlyphName(unsigned glyphIndex);
+
+  //std::string GetPostscriptName();
+    
+};
+
+
+} } // namespace avs::freetype
+
+#endif //__AVS_FREETYPE_FACE_H__

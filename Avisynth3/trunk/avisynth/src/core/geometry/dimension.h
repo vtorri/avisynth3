b5a2141bc168d6ea920d0ae76edac09af631efd6
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-08-26 16:40:54 +0000

    moved from /core

---
+++ Avisynth3/trunk/avisynth/src/core/geometry/dimension.h
@@ -0,0 +1,143 @@
+// Avisynth v3.0 alpha.  Copyright 2004 David Pierre - Ben Rudiak-Gould et al.
+// http://www.avisynth.org
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+// http://www.gnu.org/copyleft/gpl.html .
+//
+// Linking Avisynth statically or dynamically with other modules is making a
+// combined work based on Avisynth.  Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+
+
+#ifndef __AVS_DIMENSION_H__
+#define __AVS_DIMENSION_H__
+
+//avisynth includes
+#include "../roundup.h"
+#include "../exception/baddimension.h"
+
+//stl include
+#include <assert.h>
+
+
+namespace avs {
+
+
+//class declaration
+struct Vecteur;
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//  Dimension
+//
+//  helper class to represent dimensions
+//
+class Dimension
+{
+
+  int x_, y_;
+
+  static int CheckSign(int val)
+  {
+    if ( val < 0 )
+      throw exception::BadDimension();
+    return val;
+  }
+
+
+public:  //constructors
+
+  Dimension() : x_( 0 ), y_( 0 ){ }
+  Dimension(int x, int y) : x_( CheckSign(x) ), y_( CheckSign(y) ) { }
+  Dimension(Dimension const& other) : x_( other.x_ ), y_( other.y_ ) { }
+  
+  explicit Dimension(Vecteur const& vect);
+
+
+public:  //assignment
+
+  Dimension& operator=(Dimension const& other) { x_ = other.x_; y_ = other.y_; return *this; }
+
+  void swap(Dimension& other) { std::swap(x_, other.x_); std::swap(y_, other.y_); }
+
+
+public:  //access
+
+  int GetWidth() const { return x_; }
+  int GetHeight() const { return y_; }
+
+  void SetWidth(int width) { x_ = CheckSign(width); }
+  void SetHeight(int height) { y_ = CheckSign(height); }
+
+
+public:  //operators
+
+  Dimension& operator+=(Dimension const& other)
+  {
+    x_ += other.x_;
+    y_ += other.y_;
+    return *this;
+  }
+
+  Dimension& operator+=(Vecteur const& vect);
+
+
+  bool operator==(Dimension const& other) const { return x_ == other.x_ && y_ == other.y_; }
+  bool operator!=(Dimension const& other) const { return x_ != other.x_ || y_ != other.y_; }
+  
+  //special effect operator: inclusion
+  Dimension& operator >>=(Dimension const& other)
+  {
+    if ( x_ > other.x_ )
+      x_ = other.x_;
+    if ( y_ > other.y_ )
+      y_ = other.y_;
+    return *this;
+  }
+
+
+public:  //others
+
+  bool empty() const { return x_ == 0 || y_ == 0; }
+
+  template <int bps> Dimension Turn() const { assert(x_ % bps == 0); return Dimension(y_ * bps, x_ / bps); }
+
+  template <int xFactor, int yFactor> Dimension Multiply() const { return Dimension(x_ * xFactor, y_ * yFactor); }
+  template <int xFactor, int yFactor> Dimension Divide() const { return Dimension(x_ / xFactor, y_ / yFactor); }
+
+  template <int xShift, int yShift> Dimension Shift() const { return Dimension(x_ + xShift, y_ + yShift); }
+
+  template <int xRound, int yRound> Dimension Round() const { return Dimension( RoundUp<xRound>(x_), RoundUp<yRound>(y_) ); }
+
+};
+
+
+//global scope swap
+inline void swap(Dimension& left, Dimension& right) { left.swap(right); }
+
+//global scope operator+
+inline Dimension operator+(Dimension const& left, Dimension const& right)
+{
+  Dimension result(left);
+  result += right;
+  return result;
+}
+
+
+
+} //namespace avs
+
+#endif //__AVS_DIMENSION_H__
---
+++ Avisynth3/trunk/avisynth/src/core/geometry/vecteur.h
@@ -0,0 +1,110 @@
+// Avisynth v3.0 alpha.  Copyright 2004 David Pierre - Ben Rudiak-Gould et al.
+// http://www.avisynth.org
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+// http://www.gnu.org/copyleft/gpl.html .
+//
+// Linking Avisynth statically or dynamically with other modules is making a
+// combined work based on Avisynth.  Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+
+
+#ifndef __AVS_VECTEUR_H__
+#define __AVS_VECTEUR_H__
+
+//avisynth include
+#include "dimension.h"
+
+//boost include
+#include <boost/operators.hpp>
+
+
+namespace avs {
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+//  Vecteur
+//
+//
+//
+struct Vecteur
+     : boost::addable<Vecteur                      //defines Vecteur + Vecteur
+     , boost::subtractable<Vecteur                 //defines Vecteur - Vecteur
+     , boost::right_shiftable<Vecteur, Dimension>  //defines Vecteur >> Dimension (inclusion)
+       > >
+{
+
+  int x, y;   
+
+  //constructors
+  Vecteur() : x( 0 ), y( 0 ) { }
+  Vecteur(int _x, int _y) : x( _x ), y( _y ) { }
+  Vecteur(Vecteur const& other) : x( other.x ), y( other.y ) { }
+  
+  Vecteur(Dimension const& dim)  : x( dim.GetWidth() ), y( dim.GetHeight() ) { }
+
+  //operators
+  Vecteur& operator=(Vecteur const& other) { x = other.x; y = other.y; return *this; }
+
+  //classic vector operators
+  Vecteur operator-() const { return Vecteur(-x, -y); }
+  Vecteur& operator+=(Vecteur const& other) { x += other.x; y += other.y; return *this; }
+  Vecteur& operator-=(Vecteur const& other) { x -= other.x; y -= other.y; return *this; }
+  Vecteur& operator*=(int scalar) { x *= scalar; y *= scalar; return *this; }
+  Vecteur& operator/=(int scalar) { x /= scalar; y /= scalar; return *this; }
+
+
+  //operators with special effect
+  //'inclusion' operator : change self so it is included into the box defined by dim
+  Vecteur& operator >>=(Dimension const& dim)
+  {
+    x = std::max(0, std::min( x, dim.GetWidth() ));
+    y = std::max(0, std::min( y, dim.GetHeight() ));
+    return *this;
+  }
+
+  //given a pitch, returns the offset corresponding to self
+  int operator%(int pitch) const { return x + pitch * y; }  
+
+};
+
+
+inline Vecteur min(Vecteur const& left, Vecteur const& right)
+{
+  return Vecteur( std::min(left.x, right.x), std::min(left.y, right.y) );
+}
+
+inline Vecteur max(Vecteur const& left, Vecteur const& right)
+{
+  return Vecteur( std::max(left.x, right.x), std::max(left.y, right.y) );
+}
+
+
+inline Dimension::Dimension(Vecteur const& vect) : x_( CheckSign(vect.x) ), y_( CheckSign(vect.y) ) { }
+
+inline Dimension& Dimension::operator+=(Vecteur const& vect)
+{
+  CheckSign(x_ + vect.x);
+  y_ = CheckSign(y_ + vect.y);
+  x_ += vect.x;
+  return *this;
+}
+
+
+
+} //namespace avs
+
+#endif //__AVS_VECTEUR_H__

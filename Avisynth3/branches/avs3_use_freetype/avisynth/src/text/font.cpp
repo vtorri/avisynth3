93839434f12b4946eb2177090f811037ce1d0f20
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-09-03 15:36:05 +0000

    no message

--- Avisynth3/branches/avs3_use_freetype/avisynth/src/text/font.cpp
+++ Avisynth3/branches/avs3_use_freetype/avisynth/src/text/font.cpp
@@ -23,20 +23,19 @@
 
 //avisynth includes
 #include "font.h"
+//#include "../freetype/glyph.h"
+#include "../core/geometry/box.h" 
 #include "../core/exception/generic.h"
-
-//freetype includes
-#include "../freetype/glyph.h"
-#include "../freetype/face.h"
 
 
 namespace avs { namespace text {
 
 
+
 Font::Font(std::string const& name, int size)
   : font_( name, size )
 {
-  font_.SetCharSize(Dimension(0, size << 6), Dimension(300, 300));
+  font_.SetCharSize(Dimension(0, size), Dimension(300, 300));
 }
 
 
@@ -49,77 +48,27 @@ Dimension Font::GetTextBoundingBox(std::string const& text)
   bool useKerning = font_.HasKerning();
 
   unsigned prevIndex = font_.GetCharIndex(text[0]);
-  int x = font_.GetAdvance(prevIndex).x >> 6;
+
+  Glyph glyph = font_.GetGlyph(prevIndex);
+
+  Vecteur pen = glyph.GetAdvance(prevIndex);
+  Box box = glyph.GetControlBox();
 
   for( int n = 1; n < glyphCount; ++n )
   {
     unsigned glyphIndex = font_.GetCharIndex(text[n]);
 
     if ( useKerning )
-      x += font_.GetKerning(prevIndex, glyphIndex).x >> 6;
+      pen += font_.GetKerning(prevIndex, glyphIndex);
 
-  
-  freetype::Glyph *glyphs = new freetype::Glyph[num_glyphs];
-  Vecteur *pos = new Vecteur [num_glyphs];
+    glyph = font_.GetGlyph(glyphIndex);
 
-  for ( int n = 0 ; n < glyphCount; ++n )
-  {
-    Vecteur advance;
-
-    unsigned glyphIndex
-    
-    if ( use_kerning && previous && glyphIndex )
-	  {
-	    Vecteur delta = font_.GetKerning(previous, glyphIndex);
-  	  x += delta.x >> 6;
-	  }
-    
-    pos[n].x = x;
-    pos[n].y = y;
-    font_.LoadGlyph (glyph_index);
-    glyphs[n]=font_.GetGlyph (glyph_index);
-    advance = font_.GetAdvance (glyph_index);
-    x += advance.x >> 6;
-    previous = glyph_index;
+    box += glyph.GetControlBox() + pen;
+    pen += glyph.GetAdvance();
+    previousIndex = glyphIndex;
   }
 
-  Vecteur  bbox_min (32000, 32000);
-  Vecteur  bbox_max (-32000, -32000);
-
-  for ( int n = 0; n < glyphCount; ++n )
-  {
-    Vecteur min, max; 
-    glyphs[n].GetCbox (ft_glyph_bbox_pixels, &min, &max);
-    min += pos[n];
-    max += pos[n];
-
-    if ( min.x < bbox_min.x )
-	    bbox_min.x = min.x;
-    if ( min.y < bbox_min.y )
-	    bbox_min.y = min.y;
-      
-    if ( max.x > bbox_max.x )
-	    bbox_max.x = max.x;
-    if ( max.y > bbox_max.y )
-	    bbox_max.y = max.y;
-  }
-
-  if (bbox_min.x > bbox_max.x)
-    {
-      bbox_min = Vecteur (0, 0);
-      bbox_max = Vecteur (0, 0);
-    }
-
-  delete [] glyphs;
-  delete [] pos;
-
-  width  = bbox_max.x - bbox_min.x;
-  height = bbox_max.y - bbox_min.y;
-
-  if (height == 0)
-    throw exception::Generic("GetTextBoundingBox failure");
-
-  return Dimension(width, height);
+  return box.GetDimension();
 }
 
 

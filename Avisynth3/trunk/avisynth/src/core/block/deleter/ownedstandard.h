95724dc4a9deeaaf011df69a56db494216fbc18d
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-09-07 17:20:48 +0000

    no message

---
+++ Avisynth3/trunk/avisynth/src/core/block/deleter/ownedstandard.h
@@ -0,0 +1,76 @@
+// Avisynth v3.0 alpha.  Copyright 2004 David Pierre - Ben Rudiak-Gould et al.
+// http://www.avisynth.org
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+// http://www.gnu.org/copyleft/gpl.html .
+//
+// Linking Avisynth statically or dynamically with other modules is making a
+// combined work based on Avisynth.  Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+
+
+#ifndef __AVS_BLOCK_DELETER_OWNEDSTANDARD_H__
+#define __AVS_BLOCK_DELETER_OWNEDSTANDARD_H__
+
+//avisynth includes
+#include "ownedbase.h"
+#include "../align.h"
+#include "../recycler.h"
+#include "../../runtime_environment.h"
+
+
+namespace avs { namespace block { namespace deleter {
+
+
+
+class OwnedStandard : public OwnedBase
+{
+
+  bool recycle_;
+
+
+public:  //structors
+
+  OwnedStandard(int size, PEnvironment const& env, bool recycle)
+    : OwnedBase( size, env )
+    , recycle_( recycle ) { }
+
+  //generated copy constructor and destructor are fine
+
+
+public:  //deleter interface expected by block::base
+
+  enum { Align = block::Align };
+
+  //memory acquisition/release
+  BYTE * Acquire() const
+  {
+    BYTE * ptr = Recycler::instance.Acquire( size() );
+    GetEnvironment()->MemoryAllocated( size() );
+    return ptr;
+  }
+
+  void operator()(BYTE * ptr) const
+  {
+    Recycler::instance.Return(ptr, size(), recycle_);
+    GetEnvironment()->MemoryFreed( size() );
+  }
+
+};
+
+
+} } } //namespace avs::block::deleter
+
+#endif //__AVS_BLOCK_DELETER_OWNEDSTANDARD_H__

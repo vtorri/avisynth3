5687378329cf2fdb5d9e475c9dff0ec6b0a2e553
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-09-14 19:19:15 +0000

    no message

--- Avisynth3/trunk/avisynth/src/core/ownedblock.h
+++ Avisynth3/trunk/avisynth/src/core/ownedblock.h
@@ -32,7 +32,11 @@
 
 namespace avs { 
 
-  
+
+//forward declaration
+namespace block { class OwnedCreator; }
+
+
 
 ///////////////////////////////////////////////////////////////////////////////////////
 //  owned_block<align>
@@ -46,11 +50,6 @@ class owned_block : public block::base<block::OwnedHolder, align>
 {
 
 public:  //structors
-
-  owned_block(PEnvironment const& env);
-
-  //normal constructor
-  owned_block(PEnvironment const& env, int size, bool recycle);
 
   template <class Holder>
   explicit owned_block(Holder * holder)
@@ -83,14 +82,119 @@ public:  //reset methods
     swap(tmp); 
   }
 
+
+public:  //Creator typedef (helper for the buffer_window template)
+
+  typedef typename boost::enable_if<compatible<block::Align>, block::OwnedCreator>::type Creator;
+
 };
 
 
-//only defined for avs nominal align
-template <> owned_block<block::Align>::owned_block(PEnvironment const& env);
-template <> owned_block<block::Align>::owned_block(PEnvironment const& env, int size, bool recycle);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//  owned_block<block::Align>
+//
+//  specialisation of the above
+//  
+//  Just adds two constructors, allocating memory from avisynth
+//  Everything else is the same
+//
+template <>
+class owned_block<block::Align> : public block::base<block::OwnedHolder, block::Align>
+{
+
+public:  //structors
+
+  explicit owned_block(PEnvironment const& env);
+
+  //normal constructor
+  owned_block(PEnvironment const& env, int size, bool recycle);
+
+  template <class Holder>
+  explicit owned_block(Holder * holder)
+    :  BaseBlockType( holder ) { }
+
+  template <int alignOther>
+  explicit owned_block(owned_block<alignOther> const& other)
+    : BaseBlockType( other ) { }
+
+  //generated copy constructor and destructor are fine
 
 
-} //namespace avs
+public:  //assignment
+
+  template <int alignOther>
+  owned_block<block::Align>& operator=(owned_block<alignOther> const& other)
+  {
+    return static_cast<owned_block<align>&>( BaseBlockType::operator=(other) );
+  }
+
+  //generated operator= is fine
+  //swap inherited from superclass
+
+
+public:  //reset methods
+
+  void reset(int size, bool recycle)
+  { 
+    owned_block tmp(GetEnvironment(), size, recycle);
+    swap(tmp); 
+  }
+
+
+public:  //Creator typedef (helper for the buffer_window template)
+
+  typedef block::OwnedCreator Creator;
+
+};
+
+
+
+
+namespace block {
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//  block::OwnedCreator
+//
+//  This functor is used by the buffer_window template to flatten its interface.
+//
+//  Its introduction has been necessary when Buffer became a template parameter.
+//
+//  The allocating constructor now takes the buffer associated creator as parameter instead of an env
+//  And since we are implicitly convertible from an env, one can still write :
+//
+//  BufferWindow buffer(dim, env);
+//
+class OwnedCreator
+{
+
+  PEnvironment env_;
+
+
+public:  //structors
+
+  OwnedCreator(PEnvironment const& env)
+    : env_( env ) { }
+
+  template <int align>
+  OwnedCreator(owned_block<align> const& block)
+    : env_( block.GetEnvironment() ) { }
+
+  //generated copy constructor and destructor are fine
+
+
+public:  //operator(), the block creation method
+
+  owned_block<block::Align> operator()(int size, bool recycle) const 
+  { 
+    return owned_block<block::Align>(env_, size, recycle); 
+  }
+
+};
+
+
+} } //namespace avs::block
 
 #endif //__AVS_OWNEDBLOCK_H__

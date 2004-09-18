2e2ab3c1a08b13113bd3299cc865937142cbc66f
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-09-18 13:10:11 +0000

    added an include ownedblock.h

--- Avisynth3/trunk/avisynth/src/text/antialiaser.h
+++ Avisynth3/trunk/avisynth/src/text/antialiaser.h
@@ -1,4 +1,4 @@
-// Avisynth v3.0 alpha.  Copyright 2004 Ben Rudiak-Gould et al.
+// Avisynth v3.0 alpha.  Copyright 2004 David Pierre - Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
 // This program is free software; you can redistribute it and/or modify
@@ -25,6 +25,7 @@
 #define __AVS_TEXT_ANTIALASER_H__
 
 //avisynth includes
+#include "../core/ownedblock.h"
 #include "../core/bufferwindow.h"
 #include "antialiaser/bitrenderer.h"
 

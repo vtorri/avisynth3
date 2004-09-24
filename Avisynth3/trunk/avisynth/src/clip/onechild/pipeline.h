bfee9a7db69d31c13a1c80cdda22954b1fb1e0bc
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-09-24 12:03:22 +0000

    now using NOVTABLE macro

--- Avisynth3/trunk/avisynth/src/clip/onechild/pipeline.h
+++ Avisynth3/trunk/avisynth/src/clip/onechild/pipeline.h
@@ -1,4 +1,4 @@
-// Avisynth v3.0 alpha.  Copyright 2004 Ben Rudiak-Gould et al.
+// Avisynth v3.0 alpha.  Copyright 2004 David Pierre - Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
 // This program is free software; you can redistribute it and/or modify
@@ -37,7 +37,7 @@ namespace avs { namespace clip { namespace onechild {
 //
 //
 //
-class Pipeline : public VideoProcess
+class NOVTABLE Pipeline : public VideoProcess
 {
 
 protected:  //Pipeline interface

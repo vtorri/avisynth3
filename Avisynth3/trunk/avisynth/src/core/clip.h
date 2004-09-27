880535c7438825c4aced2dac43ed2cdd27790775
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2004-09-27 08:36:57 +0000

    kept Simplify name and not InitialSimplify

--- Avisynth3/trunk/avisynth/src/core/clip.h
+++ Avisynth3/trunk/avisynth/src/core/clip.h
@@ -77,7 +77,7 @@ public:  //filter chain simplication methods
   //attempts to refactor self and its childs (if there is any)
   //into a more compact filter chain
   //a default version is provided (with no refactoring)
-  virtual PClip InitialSimplify() const { return shared_from_this(); }
+  virtual PClip Simplify() const { return shared_from_this(); }
 
 
   virtual PClip FinalSimplify() const { return shared_from_this(); }

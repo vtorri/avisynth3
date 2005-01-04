036cd80a3d7e9c8dd3804ff03d559bbcfcb253fa
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2005-01-04 00:25:49 +0000

    no message

--- Avisynth3/trunk/avisynth/src/parser/scopeenforcer.h
+++ Avisynth3/trunk/avisynth/src/parser/scopeenforcer.h
@@ -1,4 +1,4 @@
-// Avisynth v3.0 alpha.  Copyright 2004 Ben Rudiak-Gould et al.
+// Avisynth v3.0 alpha.  Copyright 2004 David Pierre - Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
 // This program is free software; you can redistribute it and/or modify
@@ -28,7 +28,7 @@
 #include "vmstate.h"
 
 //boost include
-#include <boost/utility.hpp>
+#include <boost/noncopyable.hpp>   
 
 
 namespace avs { namespace parser {

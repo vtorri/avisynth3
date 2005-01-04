4787b8101772b80db4515b0eaaa05e1e104f9a3b
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2005-01-04 09:58:55 +0000

    added guards around NOMINMAX

--- Avisynth3/trunk/avisynth/src/vfw/avistream.h
+++ Avisynth3/trunk/avisynth/src/vfw/avistream.h
@@ -1,4 +1,4 @@
-// Avisynth v3.0 alpha.  Copyright 2004 David Pierre - Ben Rudiak-Gould et al.
+// Avisynth v3.0 alpha.  Copyright 2005 David Pierre - Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
 // This program is free software; you can redistribute it and/or modify
@@ -33,7 +33,9 @@
 #include <string>
 
 //windows include
+#ifndef NOMINMAX
 #define NOMINMAX          //prevents generation of min and max macros
+#endif //NOMINMAX
 #include <vfw.h>
 
 

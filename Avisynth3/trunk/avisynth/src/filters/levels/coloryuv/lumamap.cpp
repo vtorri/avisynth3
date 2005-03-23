ea4570e826761e1e8e9a7a4d5849d9d38c4582b0
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2005-03-23 18:20:38 +0000

    small optimisation

--- Avisynth3/trunk/avisynth/src/filters/levels/coloryuv/lumamap.cpp
+++ Avisynth3/trunk/avisynth/src/filters/levels/coloryuv/lumamap.cpp
@@ -27,7 +27,7 @@
 #include "../../../core/utility/saturate.h"
 
 //stl include
-#include <cmath>   //for floor, pow
+#include <cmath>   //for pow
 
 
 namespace avs { namespace filters { namespace coloryuv {
@@ -36,8 +36,9 @@ namespace avs { namespace filters { namespace coloryuv {
 
 LumaMap::LumaMap(Levels const& levels, float gamma, int (* adjust)(int), bool coring)
 {
-    
-  for ( int i = 0; i < 256; ++i )
+  BYTE * ptr = data();
+
+  for ( int i = 0; i < 256; ++i, ++ptr )
   {
     float value = static_cast<float>(adjust(i));
       
@@ -46,16 +47,10 @@ LumaMap::LumaMap(Levels const& levels, float gamma, int (* adjust)(int), bool co
     if ( gamma != 0 && value > 0 )
       value = 256 * std::pow( value / 256, 1 / std::max(0.01f, (gamma + 256) / 256) );
     
-    int val = static_cast<int>( std::floor(value + 0.5f) );
-            
-    if ( coring )
-      if ( val > 235 )
-        val = 235;
-      else
-        if ( val < 16 )
-          val = 16;
+    int val = static_cast<int>( value + 0.5f );
 
-    operator[](i) = saturate<BYTE>(val);
+    *ptr = coring ? saturate<BYTE, 16, 235>(val)
+                  : saturate<BYTE, 0, 255>(val);
   }
 
 }

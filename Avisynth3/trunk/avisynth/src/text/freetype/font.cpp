a3ae550adc56e620d55d3d4c5e69417149e39405
doursse <doursse@ae939481-8c35-0410-93bf-e308196525fa>
2005-04-01 19:49:58 +0000

    fix floating point issue

--- Avisynth3/trunk/avisynth/src/text/freetype/font.cpp
+++ Avisynth3/trunk/avisynth/src/text/freetype/font.cpp
@@ -35,7 +35,7 @@ namespace avs { namespace text { namespace freetype {
 Font::Font(std::string const& name, int size)
   : face_( Face::Create(name) )
 {
-  face_->SetCharSize( VecteurFP6(0, size << 6), DimensionFP6(300, 300) );
+  face_->SetCharSize( VecteurFP6(0, size), DimensionFP6(FP6::Wrap(300), FP6::Wrap(300)) );
 }
 
 

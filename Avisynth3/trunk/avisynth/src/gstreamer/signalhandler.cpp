d063224f5a21c60d15996b93c34970fc33993630
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2005-05-05 12:00:43 +0000

    no message

---
+++ Avisynth3/trunk/avisynth/src/gstreamer/signalhandler.cpp
@@ -0,0 +1,52 @@
+// Avisynth v3.0 alpha.  Copyright 2005 David Pierre - Ben Rudiak-Gould et al.
+// http://www.avisynth.org
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+// http://www.gnu.org/copyleft/gpl.html .
+//
+// Linking Avisynth statically or dynamically with other modules is making a
+// combined work based on Avisynth.  Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+
+
+#ifndef _WIN32
+
+//avisynth include
+#include "signalhandler.h"
+
+//glib include
+#include <glib-object.h>
+
+
+namespace avs { namespace gstreamer {
+
+
+
+SignalHandler::SignalHandler(GObject& target, char const * signalName, void (*callBack)(), void * data)
+  : target_( target )
+  , signalId_( g_signal_connect(&target, signalName, callBack, data) ) { }
+
+
+SignalHandler::~SignalHandler()
+{
+  if ( g_signal_handler_is_connected(&target_, signalId_) != 0 )
+    g_signal_handler_disconnect(&target_, signalId_);
+}
+
+
+
+} } //namespace avs::gstreamer
+
+#endif //_WIN32
---
+++ Avisynth3/trunk/avisynth/src/gstreamer/signalhandler.h
@@ -0,0 +1,66 @@
+// Avisynth v3.0 alpha.  Copyright 2005 David Pierre - Ben Rudiak-Gould et al.
+// http://www.avisynth.org
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+// http://www.gnu.org/copyleft/gpl.html .
+//
+// Linking Avisynth statically or dynamically with other modules is making a
+// combined work based on Avisynth.  Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+
+
+#ifndef __AVS_GSTREAMER_SIGNALHANDLER_H__
+#define __AVS_GSTREAMER_SIGNALHANDLER_H__
+
+//boost include
+#include <boost/noncopyable.hpp>
+
+//glib forward declaration
+typedef struct _GObject GObject;
+
+
+namespace avs { namespace gstreamer {
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//  SignalHandler
+//
+//  RAII wrapper that makes sure connected signals are disconnected
+//
+class SignalHandler : public boost::noncopyable
+{
+
+  GObject& target_;
+  unsigned long signalId_;
+
+
+public:  //structors
+
+  SignalHandler(GObject& target, char const * signalName, void (*callBack)(), void * data);
+  ~SignalHandler();
+
+
+public:  //access
+
+  GObject& GetTarget() const { return target_; }
+
+};
+
+
+
+} } //namespace avs::gstreamer
+
+#endif //__AVS_GSTREAMER_SIGNALHANDLER_H__

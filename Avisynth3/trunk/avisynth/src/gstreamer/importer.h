dd3e1872852e4de6c7e0905dc9af816399262756
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2005-06-15 13:11:01 +0000

    no message

---
+++ Avisynth3/trunk/avisynth/src/gstreamer/importer.h
@@ -0,0 +1,60 @@
+// Avisynth v3.0 alpha.  Copyright 2005 David Pierre - Ben Rudiak-Gould et al.
+// http://www.avisynth.org
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+// http://www.gnu.org/copyleft/gpl.html .
+//
+// Linking Avisynth statically or dynamically with other modules is making a
+// combined work based on Avisynth.  Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+
+
+#ifndef __AVS_GSTREAMER_IMPORTER_H__
+#define __AVS_GSTREAMER_IMPORTER_H__
+
+//avisynth includes
+#include "forward.h"                    //for PImporter
+#include "../define.h"                  //for AVS_NOVTABLE
+#include "../core/forward.h"            //for PColorSpace, Dimension, OwnedBlock
+
+//boost include
+#include <boost/shared_ptr.hpp>         //so PColorSpace, PImporter are defined
+
+
+namespace avs { namespace gstreamer {
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+//  Importer
+//
+//
+//
+class AVS_NOVTABLE Importer
+{
+
+public:  //Importer interface
+
+  virtual PColorSpace GetColorSpace() const = 0;
+
+  virtual PVideoFrame CreateFrame(Dimension const& dim, OwnedBlock const& block) const = 0;
+
+};
+
+
+
+} } //namespace avs::gstreamer
+
+#endif //__AVS_GSTREAMER_IMPORTER_H__

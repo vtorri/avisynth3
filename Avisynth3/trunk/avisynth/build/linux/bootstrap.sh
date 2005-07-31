13919e443ddf76fee979c00b40146412fa7d60d2
doursse <doursse@ae939481-8c35-0410-93bf-e308196525fa>
2005-07-31 16:50:06 +0000

    BSD support. almost complete rewrite, so that it's beautiful, now :)

--- Avisynth3/trunk/avisynth/build/linux/bootstrap.sh
+++ Avisynth3/trunk/avisynth/build/linux/bootstrap.sh
@@ -3,6 +3,7 @@
 # - Bootstrap script -
 #
 # Copyright(C) 2003-2004 Edouard Gomez <ed.gomez@free.fr>
+# Copyright(C) 2004-2005 Vincent Torri <torri@iecn.u-nancy.fr>
 #
 # This file builds the configure script and copies all needed files
 # provided by automake/libtoolize
@@ -10,74 +11,142 @@
 # $Id$
 
 
+rm -rf autom4te.cache 1>/dev/null 2>&1
+rm -f aclocal.m4 1>/dev/null 2>&1
+
+##############################################################################
+# Helper functions
+##############################################################################
+
+set_package_bsd()
+{
+# Check if the package exists (by checking the version),
+# and set the variable PACKAGE to the package name
+#
+# argument 1: name of the package
+# argument 2: major version
+# argument 3: minimum minor version
+# argument 4: set of minor versions
+
+    NAME=$1
+    MAJOR=$2
+    MINOR=$3
+    SET=$4
+
+    PACKAGE=""
+    for minor in $SET
+    do
+      name=$NAME$MAJOR$minor
+      $name --version 1>/dev/null 2>&1
+      if test "$?" -eq "0" ; then
+	  PACKAGE=$name
+      fi
+    done
+    if test "x$PACKAGE" = "x" ; then
+	echo ""
+	echo "Error: $NAME >= $MAJOR.$MINOR required"
+	exit 127
+    else
+	echo "     + $NAME: $PACKAGE"
+    fi
+}
+
+set_package_linux()
+{
+# Check if the package exists (by checking the version),
+# and set the variable PACKAGE to the package name
+#
+# argument 1: name of the package
+# argument 2: minimum major version
+# argument 3: minimum minor version
+
+    NAME=$1
+    MAJOR=$2
+    MINOR=$3
+
+    VER=`$NAME --version | head -1 | sed 's/'^[^0-9]*'/''/'`
+    MAJORVER=`echo $VER | cut -f1 -d'.'`
+    MINORVER=`echo $VER | cut -f2 -d'.'`
+    if [ "$MAJORVER" -lt "$MAJOR" ]; then
+	echo ""
+	echo "Error: $NAME >= $MAJOR.$MINOR required (found $VER)"
+	exit -1
+    fi
+    if [ "$MINORVER" -lt "$MINOR" ]; then
+	echo ""
+	echo "Error: $NAME >= $MAJOR.$MINOR required (found $VER)"
+	exit -1
+    fi
+    echo "     + $NAME: $NAME $MAJORVER.$MINORVER"
+}
+
 ##############################################################################
 # Detect the right autoconf script
 ##############################################################################
 
-# Find a suitable autoconf
-AUTOCONF="autoconf2.50"
-$AUTOCONF --version 1>/dev/null 2>&1
+# Check for the OS. They all have specific versioning of the autotools
+OS=`uname -s`
 
-if [ $? -ne 0 ] ; then
-    AUTOCONF="autoconf"
-    $AUTOCONF --version 1>/dev/null 2>&1
+echo " + Checking autotools versions"
+echo "     + OS: $OS"
 
-	if [ $? -ne 0 ] ; then
-        echo "ERROR: 'autoconf' not found"
-        exit -1
-    fi
-fi
+AUTOCONF=""
+AC_MAJOR=2
+AC_MINOR=50
 
-# Tests the autoconf version
-AC_VER=`$AUTOCONF --version | head -1 | sed 's/'^[^0-9]*'/''/'`
-AC_MAJORVER=`echo $AC_VER | cut -f1 -d'.'`
-AC_MINORVER=`echo $AC_VER | cut -f2 -d'.'`
+ACLOCAL=""
+AL_MAJOR=1
+AL_MINOR=7
 
-if [ "$AC_MAJORVER" -lt "2" ]; then
-    echo "ERROR: This bootstrapper requires Autoconf >= 2.50 (detected $AC_VER)"
-    exit -1
-fi
+AUTOMAKE=""
+AM_MAJOR=1
+AM_MINOR=7
 
-if [ "$AC_MINORVER" -lt "50" ]; then
-    echo "ERROR: This bootstrapper requires Autoconf >= 2.50 (detected $AC_VER)"
-    exit -1
-fi
+LIBTOOLIZE=""
+LT_MAJOR=1
+LT_MINOR=4
 
-LIBTOOLIZE="libtoolize"
-$LIBTOOLIZE --version 1>/dev/null 2>&1
-
-if [ $? -ne 0 ] ; then
-    LIBTOOLIZE="glibtoolize"
-    $LIBTOOLIZE --version 1>/dev/null 2>&1
-
-    if [ $? -ne 0 ] ; then
-        echo "ERROR: 'libtoolize' not found"
-        exit -1
-    fi
-fi
-
-AUTOMAKE="automake"
-$AUTOMAKE --version 1>/dev/null 2>&1
-
-if [ $? -ne 0 ] ; then
-    echo "ERROR: 'automake' not found"
-	exit -1
-fi
+case $OS in
+    FreeBSD)
+	set_package_bsd "autoconf" "$AC_MAJOR" "$AC_MINOR" "50 51 52 53 54 55 56 57 58 59"
+	AUTOCONF=$PACKAGE
+	set_package_bsd "aclocal" "$AL_MAJOR" "$AL_MINOR" "7 8 9"
+	ACLOCAL=$PACKAGE
+	set_package_bsd "automake" "$AM_MAJOR" "$AM_MINOR" "7 8 9"
+	AUTOMAKE=$PACKAGE
+	set_package_bsd "libtoolize" "$LT_MAJOR" "$LT_MINOR" "4 5"
+	LIBTOOLIZE=$PACKAGE
+	;;
+    Linux)
+	set_package_linux "autoconf" "$AC_MAJOR" "$AC_MINOR"
+	AUTOCONF="autoconf"
+	set_package_linux "aclocal" "$AL_MAJOR" "$AL_MINOR"
+	ACLOCAL="aclocal"
+	set_package_linux "automake" "$AM_MAJOR" "$AM_MINOR"
+	AUTOMAKE="automake"
+	set_package_linux "libtoolize" "$LT_MAJOR" "$LT_MINOR"
+	LIBTOOLIZE="libtoolize"
+	;;
+    *)
+	echo "Operating system not supported."
+	echo "Send a mail to <torri@iecn.u-nancy.fr>"
+	;;
+esac
 
 ##############################################################################
-# Bootstraps the configure script
+# Bootstrap the configure script
 ##############################################################################
 
 echo " + Creating ./configure"
-aclocal -I ./m4
+$ACLOCAL -I ./m4
 $AUTOCONF
 
 echo " + Copying files provided by automake"
-$AUTOMAKE -c -a 1>/dev/null 2>&1
+$AUTOMAKE -c -a
 
 echo " + Copying files provided by libtool"
-$LIBTOOLIZE -f -c 1>/dev/null 2>&1
+$LIBTOOLIZE -f -c
 
 echo " + Removing files that are not needed"
-rm -rf autom4* 1>/dev/null 2>&1 
-rm -rf ltmain.sh 1>/dev/null 2>&1 
+rm -rf autom4* 1>/dev/null 2>&1
+rm -rf ltmain.sh 1>/dev/null 2>&1

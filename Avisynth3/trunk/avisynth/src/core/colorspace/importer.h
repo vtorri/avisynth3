0f0b4c46acdb0b9f35203decae8bd2b690dae9ba
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2005-10-08 16:08:52 +0000

    no message

--- Avisynth3/trunk/avisynth/src/core/colorspace/importer.h
+++ Avisynth3/trunk/avisynth/src/core/colorspace/importer.h
@@ -1,4 +1,4 @@
-// Avisynth v3.0 alpha.  Copyright 2005 David Pierre - Ben Rudiak-Gould et al.
+// Avisynth v3.0 alpha.  Copyright 2003-2005 David Pierre - Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
 // This program is free software; you can redistribute it and/or modify
@@ -57,7 +57,7 @@ public:  //Importer interface
   virtual PColorSpace GetColorSpace() const = 0;
 
   //creates a frame of given dimension using data from block
-  virtual CPVideoFrame CreateFrame(owned_block<1> const& block, Dimension const& dim, FrameType type) const = 0;
+  virtual CPVideoFrame CreateFrame(Dimension const& dim, FrameType type, owned_block<1> const& block, long offset) const = 0;
 
 };
 

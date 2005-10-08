64d72cd50e6ee6cb27cac5b431c226da71d06fd3
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2005-10-08 23:40:55 +0000

    no message

--- Avisynth3/trunk/avisynth/src/export/vfw/forward.h
+++ Avisynth3/trunk/avisynth/src/export/vfw/forward.h
@@ -1,4 +1,4 @@
-// Avisynth v3.0 alpha.  Copyright 2005 David Pierre - Ben Rudiak-Gould et al.
+// Avisynth v3.0 alpha.  Copyright 2003-2005 David Pierre - Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
 // This program is free software; you can redistribute it and/or modify
@@ -22,7 +22,7 @@
 
 
 #ifndef __AVS_EXPORT_VFW_FORWARD_H__
-#define __AVS_VFW_FORWARD_H__
+#define __AVS_EXPORT_VFW_FORWARD_H__
 
 
 namespace boost { template <typename T> class shared_ptr; }
@@ -33,15 +33,12 @@ namespace avs { namespace export_ { namespace vfw {
 
 
 //declarations
+class AviFile;
 class Exporter;
 class AviStream;
-class WaveFormatEx;
-class BitmapInfoHeader;
 
 //typedefs
 typedef boost::shared_ptr<Exporter const> PExporter;
-typedef boost::shared_ptr<WaveFormatEx> PWaveFormatEx;
-typedef boost::shared_ptr<BitmapInfoHeader> PBitmapInfoHeader;
 
 
 

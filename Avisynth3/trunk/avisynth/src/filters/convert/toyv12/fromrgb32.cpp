5ae0cce8698dd6026705db29dac36b40198a559d
doursse <doursse@ae939481-8c35-0410-93bf-e308196525fa>
2005-10-23 21:25:56 +0000

    and now, the U and V planes are implemented

--- Avisynth3/trunk/avisynth/src/filters/convert/toyv12/fromrgb32.cpp
+++ Avisynth3/trunk/avisynth/src/filters/convert/toyv12/fromrgb32.cpp
@@ -52,6 +52,17 @@ void FromRGB32::ConvertFrame(CWindowPtr src, WindowPtr Y, WindowPtr U, WindowPtr
   long GtoYCoeff = long (129.057 * 256 + 0.5);
   long BtoYCoeff = long (25.064  * 256 + 0.5);
 
+  long RtoUCoeff = long (-37.945 * 256 + 0.5);
+  long GtoUCoeff = long (-74.494 * 256 + 0.5);
+  long BtoUCoeff = long (112.439 * 256 + 0.5);
+
+  long RtoVCoeff = long (112.439 * 256 + 0.5);
+  long GtoVCoeff = long (-94.154 * 256 + 0.5);
+  long BtoVCoeff = long (-18.285 * 256 + 0.5);
+
+  BYTE U00, U01, U10, U11;
+  BYTE V00, V01, V10, V11;
+
   //Y plane
   for ( int y = src.height; y-- > 0; Y.pad(), src.pad() )
     for ( int x = src.width / 4; x-- > 0; src.to(4, 0), Y.to(1, 0) )
@@ -60,7 +71,25 @@ void FromRGB32::ConvertFrame(CWindowPtr src, WindowPtr Y, WindowPtr U, WindowPtr
       Y[0] = 16 + ((32768 + RtoYCoeff * src[0] + GtoYCoeff * src[1] + BtoYCoeff * src[2]) >> 16);
     }
 
+  src.toTop();
+
   //U and V planes : to be done
+  for ( int y = U.height; y-- > 0; U.pad(), V.pad(), src.skipPad() )
+    for ( int x = U.width; x-- > 0; U.to(1, 0), V.to(1, 0), src.to(8, 0) )
+    {
+      // No need to saturate between 16 and 240
+      U00 = 128 + ((32768 + RtoUCoeff * src[0] + GtoUCoeff * src[1] + BtoUCoeff * src[2]) >> 16);
+      U01 = 128 + ((32768 + RtoUCoeff * src[4] + GtoUCoeff * src[5] + BtoUCoeff * src[6]) >> 16);
+      U10 = 128 + ((32768 + RtoUCoeff * src(0, 1) + GtoUCoeff * src(1, 1) + BtoUCoeff * src(2, 1)) >> 16);
+      U11 = 128 + ((32768 + RtoUCoeff * src(4, 1) + GtoUCoeff * src(5, 1) + BtoUCoeff * src(6, 1)) >> 16);
+      U[0] = (U00 + U01 + U10 + U11) >> 2;
+
+      V00 = 128 + ((32768 + RtoVCoeff * src[0] + GtoVCoeff * src[1] + BtoVCoeff * src[2]) >> 16);
+      V01 = 128 + ((32768 + RtoVCoeff * src[4] + GtoVCoeff * src[5] + BtoVCoeff * src[6]) >> 16);
+      V10 = 128 + ((32768 + RtoVCoeff * src(0, 1) + GtoVCoeff * src(1, 1) + BtoVCoeff * src(2, 1)) >> 16);
+      V11 = 128 + ((32768 + RtoVCoeff * src(4, 1) + GtoVCoeff * src(5, 1) + BtoVCoeff * src(6, 1)) >> 16);
+      V[0] = (V00 + V01 + V10 + V11) >> 2;
+    }
 }
 
 

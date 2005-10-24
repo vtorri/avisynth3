6d89f6fad839a8b6f54dcd85c8a44f82f59f88f9
doursse <doursse@ae939481-8c35-0410-93bf-e308196525fa>
2005-10-24 16:49:35 +0000

    i've missed the point of the +2... Now I got it :)

--- Avisynth3/trunk/avisynth/src/filters/convert/toyv12/fromrgb32.cpp
+++ Avisynth3/trunk/avisynth/src/filters/convert/toyv12/fromrgb32.cpp
@@ -82,13 +82,13 @@ void FromRGB32::ConvertFrame(CWindowPtr src, WindowPtr Y, WindowPtr U, WindowPtr
       U01 = 128 + ((32768 + RtoUCoeff * src[4] + GtoUCoeff * src[5] + BtoUCoeff * src[6]) >> 16);
       U10 = 128 + ((32768 + RtoUCoeff * src(0, 1) + GtoUCoeff * src(1, 1) + BtoUCoeff * src(2, 1)) >> 16);
       U11 = 128 + ((32768 + RtoUCoeff * src(4, 1) + GtoUCoeff * src(5, 1) + BtoUCoeff * src(6, 1)) >> 16);
-      U[0] = 2 + ((U00 + U01 + U10 + U11) >> 2);
+      U[0] = (2 + U00 + U01 + U10 + U11) >> 2;
 
       V00 = 128 + ((32768 + RtoVCoeff * src[0] + GtoVCoeff * src[1] + BtoVCoeff * src[2]) >> 16);
       V01 = 128 + ((32768 + RtoVCoeff * src[4] + GtoVCoeff * src[5] + BtoVCoeff * src[6]) >> 16);
       V10 = 128 + ((32768 + RtoVCoeff * src(0, 1) + GtoVCoeff * src(1, 1) + BtoVCoeff * src(2, 1)) >> 16);
       V11 = 128 + ((32768 + RtoVCoeff * src(4, 1) + GtoVCoeff * src(5, 1) + BtoVCoeff * src(6, 1)) >> 16);
-      V[0] = 2 + ((V00 + V01 + V10 + V11) >> 2);
+      V[0] = (2 + V00 + V01 + V10 + V11) >> 2;
     }
 }
 

30a3e5ce78060e9e16cd2392bdf38be51f641023
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2006-07-29 18:12:01 +0000

    Corrected namespace from export to export_

--- Avisynth3/trunk/avisynth/src/export/vfw/base.h
+++ Avisynth3/trunk/avisynth/src/export/vfw/base.h
@@ -1,4 +1,4 @@
-// Avisynth v3.0 alpha.  Copyright 2003-2005 David Pierre - Ben Rudiak-Gould et al.
+// Avisynth v3.0 alpha.  Copyright 2003-2006 David Pierre - Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
 // This program is free software; you can redistribute it and/or modify
@@ -36,7 +36,7 @@
 #include <objbase.h>
 
 
-namespace avs { namespace export { namespace vfw {
+namespace avs { namespace export_ { namespace vfw {
 
 
 //declaration and typedef

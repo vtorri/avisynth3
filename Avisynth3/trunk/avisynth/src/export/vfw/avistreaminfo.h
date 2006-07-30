e1fe6a354edf1a978db24e4622bf3b2c7abd8032
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2006-07-30 15:31:02 +0000

    added some comments

--- Avisynth3/trunk/avisynth/src/export/vfw/avistreaminfo.h
+++ Avisynth3/trunk/avisynth/src/export/vfw/avistreaminfo.h
@@ -1,4 +1,4 @@
-// Avisynth v3.0 alpha.  Copyright 2003-2005 David Pierre - Ben Rudiak-Gould et al.
+// Avisynth v3.0 alpha.  Copyright 2003-2006 David Pierre - Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
 // This program is free software; you can redistribute it and/or modify
@@ -38,7 +38,10 @@ namespace avs { namespace export_ { namespace vfw {
 /////////////////////////////////////////////////////////////////////////////////////
 //  AviStreamInfo
 //
+//  avs C++ version of the VFW struct AVISTREAMINFOW
 //
+//  Though they are not related through inheritance,
+//  the layout is the same, so cast inbetween are safe
 //
 struct AviStreamInfo : public import::avi::StreamHeader
 {

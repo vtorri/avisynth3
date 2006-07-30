4e52c9e8bac88c23b67115e4b196bbd9fa58b289
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2006-07-30 01:59:19 +0000

    imported namespace avs::export_::vfw into scope

--- Avisynth3/trunk/avisynth/src/export/vfw/main.cpp
+++ Avisynth3/trunk/avisynth/src/export/vfw/main.cpp
@@ -1,4 +1,4 @@
-// Avisynth v3.0 alpha.  Copyright 2003-2005 David Pierre - Ben Rudiak-Gould et al.
+// Avisynth v3.0 alpha.  Copyright 2003-2006 David Pierre - Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
 // This program is free software; you can redistribute it and/or modify
@@ -28,13 +28,14 @@
 #include "main.h"
 #include "avifilefactory.h"
 
+using namespace avs::export_::vfw;
 
 
 //methods required of a COM dll
 
 BOOL APIENTRY DllMain(HANDLE /*hModule*/, ULONG ulReason, LPVOID /*lpReserved*/) 
 {
-	switch(ulReason) 
+  switch(ulReason) 
   {
 	case DLL_PROCESS_ATTACH:
 		CoInitialize(NULL);

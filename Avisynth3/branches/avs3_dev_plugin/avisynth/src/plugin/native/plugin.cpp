78bedaf22534c441672f9fdc0e97c3caa10580cf
bidoche <bidoche@ae939481-8c35-0410-93bf-e308196525fa>
2006-09-19 21:27:28 +0000

    code for the native Linker

---
+++ Avisynth3/branches/avs3_dev_plugin/avisynth/src/plugin/native/plugin.cpp
@@ -0,0 +1,46 @@
+// Avisynth v3.0 alpha.  Copyright 2003-2005 David Pierre - Ben Rudiak-Gould et al.
+// http://www.avisynth.org
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+// http://www.gnu.org/copyleft/gpl.html .
+//
+// Linking Avisynth statically or dynamically with other modules is making a
+// combined work based on Avisynth.  Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+
+
+//avisynth include
+#include "plugin.h"
+
+
+namespace avs { namespace plugin { namespace native {
+
+
+
+namespace
+{
+  enum { name, versionLabel, count };
+  char const * symbols[count] = { "GetName", "GetVersionLabel" };
+}
+
+
+Plugin::Plugin(LibraryHandle const& handle)
+  : handle_( handle )
+{
+  for (int i = count; i-- > 0; )
+    handle_.CheckSymbolExists( symbols[i] );
+}
+
+
